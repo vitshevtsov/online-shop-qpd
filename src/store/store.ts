e541065241680d01cd55c8b88a7b770acaf4807b
admin <admin-astra@astra>
2022-04-20 18:13:21 +0300

    feat: creating categories slice

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,10 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
+import categoriesReducer from '../store/categoriesSlice'
 
 export const store = configureStore({
-    reducer: {},
+    reducer: {
+        categories: categoriesReducer,
+    },
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-export type AppDispatch = typeof store.dispatch
+export type AppDispatch = typeof store.dispatch 
